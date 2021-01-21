3f40a9db3eb7fde1cedac998971c55e963d09d6c
leonwormuth <leon.wormuth123@gmail.com>
2021-01-21 19:14:13 +0800

    Add JUnit & tests

---
+++ LCalendar/src/lcalendar/LCalendarTest.java
@@ -0,0 +1,48 @@
+package lcalendar;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class LCalendarTest {
+	
+	@Test
+	void getYearTest() {
+		LCalendar cal = new LCalendar();
+		MonthArray m = cal.now();
+		int result = m.getYear();
+		assertEquals(2021, result);
+	}
+	
+	@Test
+	void getFirstTest() {
+		LCalendar cal = new LCalendar();
+		MonthArray m = cal.now();
+		int result = m.getFirst();
+		assertEquals(11, result);
+	}
+	
+	@Test
+	void prevMonthTest() {
+		LCalendar cal = new LCalendar();
+		MonthArray m = cal.prevMonth();
+		int result = m.getMonth();
+		assertEquals(11, result);
+	}
+	
+	@Test
+	void nowTest() {
+		LCalendar cal = new LCalendar();
+		MonthArray m = cal.now();
+		int result = m.getMonth();
+		assertEquals(0, result);
+	}
+	
+	@Test
+	void nextMonthTest() {
+		LCalendar cal = new LCalendar();
+		MonthArray m = cal.nextMonth();
+		int result = m.getMonth();
+		assertEquals(1, result);
+	}
+}
